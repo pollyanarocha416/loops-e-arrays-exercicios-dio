249d8e0aaa858ddaa6cd8cd58e687c4d6921da33
Pollyana <pollyanarocha416@gmail.com>
2023-01-03 19:32:15 -0400

    reforcando os conhecimentos em do while

--- src/br/com/dio/exercicios/loops/Ex4_ParEImpar.java
+++ src/br/com/dio/exercicios/loops/Ex4_ParEImpar.java
@@ -11,23 +11,24 @@ public class Ex4_ParEImpar {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int quantNumeros;
+        int quantNumeros;//qtd de numeros q eu quero
         int numero;
         int quantPares = 0, quantImpares = 0;
 
         System.out.println("Quantidade de números: ");
         quantNumeros = scan.nextInt();
 
-        int count = 0;
+        //o contador primeiro
+        int count = 0;// pra parar o loop
         do {
             System.out.println("Número: ");
             numero = scan.nextInt();
-
-            if (numero % 2 == 0 ) quantPares++;
+//          pra verificar e incrementa o val de impar e par
+            if (numero % 2 == 0 ) quantPares++;//vai ser atribuido 1 ao quantPares, pra contar os nums de par
             else quantImpares++;
 
-            count++;
-        } while(count < quantNumeros);
+            count++;// ele vai fica a vida inteira se nao tiver ele
+        } while(count < quantNumeros);// a qtd que foi enformada em qtdd
 
         System.out.println("Quantidade Par: " + quantPares);
         System.out.println("Quantidade Ímpar: " + quantImpares);
