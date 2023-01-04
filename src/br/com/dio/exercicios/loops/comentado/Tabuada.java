ae61c3f68d93a5bcea30fa48e350a123bc918e3d
Pollyana <pollyanarocha416@gmail.com>
2023-01-03 20:50:48 -0400

    for incrementando

--- src/br/com/dio/exercicios/loops/comentado/Tabuada.java
+++ src/br/com/dio/exercicios/loops/comentado/Tabuada.java
@@ -17,13 +17,13 @@ public class Tabuada {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in); //Abertura da entrada de fluxo de dados através do teclado
 
-        System.out.println("Número: "); //Peça um número
+        System.out.print("Número: "); //Peça um número
         int numero = scan.nextInt(); //Entrada de dados, neste caso de um int.
 
-        System.out.println("Tabuada de " + numero + ":");//frase ilustrativa para indicar a tabuada
+        System.out.println("Tabuada do " + numero + ":");//frase ilustrativa para indicar a tabuada
 
         //para: variável i = 0, até i menor ou igual a 10, adicionando 1 ao valor de i a cada loop.
-        for (int i = 0; i <= 10; i = i + 1) {
+        for (int i = 1; i <= 10; i++) {
             //número escolhido pelo usuário "X" o valor de i "=" a multiplicação do número com o i.
             System.out.println(numero + " X " + i + " = " + (numero * i));
         }
